16e1e4d0fea438209b8494707fc0d163875bcdcd
Giovanni Chiarpenello <giovanni.chiarpenello@alten.it>
2023-11-28 10:06:52 +0100

    rimossi tutti i file da fe

--- progettoFormazione/src/app/guards/auth.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivateFn } from '@angular/router';
-
-export const authGuard: CanActivateFn = (route, state) => {
-  const ver = localStorage.getItem('token');
-  if(!ver){
-    return false;
-  }else
-    return true;
-};
