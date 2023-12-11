858ec2bf8e9ba965544fcb4b90c9bece8e3b3ffe
PietroMateria <pietromateria1193@gmail.com>
2023-12-11 10:54:46 +0100

    modifiche be

---
+++ moduls/db.js
@@ -0,0 +1,33 @@
+'use strict'
+
+var mariadb = require('mariadb');
+ 
+// Create a connection pool
+var pool = 
+  mariadb.createPool({
+    host:"localhost", 
+    user:"root", 
+    password: "root",
+    database:"libreria"
+  });
+
+  async function executeQuery(sql, res) {
+
+    return new Promise(async (resolve, reject) => {
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            return resolve(await conn.query(sql, res));
+            conn.close()
+            
+        } catch (err) {
+            reject(err)
+        }
+        conn && conn.release()
+    });
+
+}
+
+executeQuery('select 1 as hello').then(console.log).catch(console.err);
+//metodo per esporre la connessione al db
+module.exports = executeQuery
--- server.js
+++ server.js
@@ -6,29 +6,42 @@ const express= require("express")
 const app= express()
 const flash= require("express-flash")
 const session= require("express-session")
-const executeQuery= require("C:/Users/pmateria/Desktop/ProgettoStage/moduls/db.js")
-const middleware= require("C:/Users/pmateria/Desktop/ProgettoStage/moduls/middleware.js")
+const executeQuery= require("./moduls/db")
+const mariadb= require('mariadb')
 const cors= require('cors')
+
 
 app.use(express.json())   // uso di json
 app.use(express.urlencoded({extended: true}))
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
 // Post registrazione utente in tabella utenti. Il json richiede come paramtri Username,Email,Password,Nome,Cognome li acquisisce e li invia al database 
-app.post("/registrazione", async function (req, res, next){
+app.post("/registrazione", function (req, res, next){
     const Username= req.body.Username
     const Email= req.body.Email
     const Password= req.body.Password
     const Cognome= req.body.Cognome
     const Nome= req.body.Nome
-    const rows = await executeQuery("SELECT * from utenti");
-    if(res.length>0){
-        res.send("La mail inserita è esistente!");
-    }else {
+    
+    const rows = executeQuery(`SELECT * from utenti WHERE Email= "${Email}" `);
+    rows.then(function(email,username){
+    if(email == 0 || username == 0){
         executeQuery(`INSERT into utenti(Username,Email,Password,Cognome,Nome) values('${Username}','${Email}','${Password}','${Cognome}','${Nome}')`,function(er,res){
-        res.send("Utente registrato con successo")
-    });
-    console.log(rows)
-    }})
+            res.send("Utente registrato con successo")
+            res.send(rows)
+        })
+    }else {
+        res.send("Email o Username non validi!");
+    }
+})})
+
+    
 
     //Delete di un user con identificativo id per la rimozione del record 
     app.get('/user/delete/:id', function(req,res,next){
@@ -43,7 +56,7 @@ app.post("/registrazione", async function (req, res, next){
         });
     })
 
-// Post login utente in tabella utenti. Il json richiede come paramtri Username,Email,Password li acquisisce e li invia al database tramite query Insert 
+// Post LOGIN utente in tabella utenti. Il json richiede come paramtri Username,Email,Password li acquisisce e li invia al database tramite query Insert 
 app.post("/login", function(request,result, next){
         const username= request.body.username
         const email= request.body.Email
@@ -58,4 +71,25 @@ app.post("/login", function(request,result, next){
     console.log(rows)
 }
 })
+// AGGIUNTA DI UN LIBRO
+app.post("/registrazione-libro", function(request,result,next){
+    const isbn= request.body.isbn
+    const titolo= request.body.titolo
+    const NomeAutore= request.body.NomeAutore
+    const CognomeAutore= request.body.CognomeAutore
+    const Genere= request.body.Genere
+    const Lingua= request.body.Lingua
+    const PosizioneInLibreria= request.body.PosizioneInLibreria
+
+    const rows = executeQuery("SELECT * from login ");
+    if (isbn.length != 13){
+        result.send("Il codice ISBN inserito è sbagliato!") 
+    }
+    else{
+     executeQuery(`INSERT into libri(isbn,Titolo,NomeAutore,CognomeAutore,Genere,Lingua,PosizioneInLibreria) values('${isbn}','${titolo}','${NomeAutore}','${CognomeAutore}','${Genere}','${Lingua}','${PosizioneInLibreria}')`,function(er,res){
+     result.send("Libro registrato")    
+    })
+}})
+
+
 app.listen(23456);
