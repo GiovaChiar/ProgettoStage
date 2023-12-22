d2768b5ec943481762a9539f956af49d391290b2
PietroMateria <pietromateria1193@gmail.com>
2023-12-22 16:42:55 +0100

    add sezione prestiti

--- moduls/db.js
+++ moduls/db.js
@@ -10,17 +10,17 @@ var pool = mariadb.createPool({
   database:"libreria"
 });
 
-async function executeQuery(sql, params = []) {
-	let conn;
-    try {
-    	conn = await pool.getConnection();
-      return conn.query(sql, params);
-    } catch (err) {
-    	if (conn) {
-        conn.release(); //release to pool
+  async function executeQuery(sql, params = []) {
+    let conn;
+      try {
+        conn = await pool.getConnection();
+        return conn.query(sql, params);
+      } catch (err) {
+        if (conn) {
+          conn.release(); //release to pool
+      }
     }
-	}
-}
+  }
 
 //metodo per esporre la connessione al db
 module.exports = executeQuery
--- server.js
+++ server.js
@@ -12,16 +12,20 @@ const jwt = require('jsonwebtoken');
 
 app.use(cors())
 
+
 app.use(express.json())   // uso di json
 app.use(express.urlencoded({extended: true}))
 
-
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if ('OPTIONS' == req.method) {
+       res.sendStatus(200);
+     }
+     else {
+       next();
+     }});
   
 // AUTENTICAZIONE UTENTE
 // ROUTH DI REGISTRAZIONE DI UN UTENTE
@@ -41,21 +45,21 @@ app.post("/registrazione", async function (req, res, next) {
     if(emailRows.length == 0) {
          if(Password != "") {
          await executeQuery(insertQuery, [Username, Email, Password, Cognome, Nome]);
-         res.json({utente: "UTENTE REGISTRATO"});
+         res.json({message: "UTENTE REGISTRATO"});
          } else {
-            res.json({passwordVuota: "Campo password vuoto"});
+            res.json({message: "Campo password vuoto"});
         }
      } 
     else if (Email == "") {
-        res.json({emailVuota: "Campo email vuoto"});
+        res.json({message: "Campo email vuoto"});
     } else if (Username == "") {
-         res.json({usernameVuota: "Campo username vuoto"});
+         res.json({message: "Campo username vuoto"});
      } else {
-         res.json({emailEUsernameVuoti: "Email e Username esistenti"});
+         res.json({message: "Email e Username esistenti"});
       } 
      } catch (error) {
         console.log(error);
-        res.status(500).json({error: "Errore del server"});
+        res.status(500).json({message: "Errore del server"});
     }
 })
 
@@ -64,13 +68,13 @@ app.post("/registrazione", async function (req, res, next) {
 app.post('/login', (req, res) => {
     const { username, password, email } = req.body;
     // Verifica se tutti i campi sono presenti
-    if (username && password && email) {
+    if (username || password && email) {
       // Cerca l'utente nella tabella
-      const utente = "SELECT * FROM utenti WHERE username=? AND passwoerd=? AND email=?"
+      const utente = "SELECT * FROM utenti WHERE username=? AND password=? AND email=?"
   
       if (utente) {
         // Genera un token JWT
-        const token = jwt.sign({ passwoerd: utente.passwoerd }, 'chiaveSegretaDelToken');
+        const token = jwt.sign({ password: utente.password }, 'chiaveSegretaDelToken');
   
         res.json({ token }); // Invia il token al client
       } else {
@@ -89,13 +93,13 @@ if(ruolo == role.Amministatore){
 //DELETE CON PATH PARAM PER CANCELLARE UN UTENTE TRAMITE ID
   app.delete('/user/delete/:id', function(req,res,next){
     
-    var id= req.params.id   
-        var query= `DELETE FROM utenti WHERE id = "${id}"`
+    const id= req.params.id   
+    const query= `DELETE FROM utenti WHERE id = "${id}"`
         executeQuery(query,function(error,data){
             if(error){
                 throw err;
             }else{
-                result.json({utente: "Utente cancellato"}) 
+                result.json({message: "Utente cancellato"}) 
             }
                 })
     })
@@ -115,24 +119,27 @@ app.post("/registrazione-libro", function(request,result,next){
     const PosizioneInLibreria= request.body.PosizioneInLibreria
     const NumeroDiCopie= request.body.NumeroDiCopie
 
-    const rows = executeQuery("SELECT * from libri ");
+    const rows = executeQuery("SELECT * FROM libri WHERE isbn = ?", [isbn]);
     
     if (isbn.length != 13){
-        result.json({libro: "Il codice ISBN inserito è sbagliato!"}) 
+    result.json({message: "Il codice ISBN inserito è sbagliato!"}) 
     }
     else{
-    executeQuery(`INSERT into libri(isbn,titolo,NomeAutore,CognomeAutore,Genere,Lingua,PosizioneInLibreria,NumeroDiCopie) values('${isbn}','${titolo}','${NomeAutore}','${CognomeAutore}','${Genere}','${Lingua}','${PosizioneInLibreria}','${NumeroDiCopie}')`).then(() => {
-    result.json({libro: "Libro registrato"})
+    executeQuery(`INSERT into libri(isbn,titolo,NomeAutore,CognomeAutore,Genere,Lingua,PosizioneInLibreria,NumeroDiCopie) VALUES(?,?,?,?,?,?,?,?)`, [isbn, titolo, NomeAutore, CognomeAutore, Genere, Lingua, PosizioneInLibreria, NumeroDiCopie]).then(() => {
+        result.json({message: "Libro registrato"})
     })
-}}) 
+}
+})
 
-// GET RISERVATA ALL'ADMIN E AGLI UTENTI ANCHE NON REGISTRATI
+// AREA RISERVATA ALL'ADMIN E AGLI UTENTI ANCHE NON REGISTRATI
 if(ruolo!= role.Amministatore  || ruolo== role.Amministatore){
 
 // GET PER L'INTERA L'ISTA DEI LIBRI
-    app.get("/listaLibri", async(req, res) => {
-    res.json(await executeQuery("SELECT * FROM libri"))
-    })
+app.get("/listaLibri", async (req, res) => {
+    const query = "SELECT * FROM libri";
+    const result = await executeQuery(query);
+    res.json({message: "Libro registrato", result})
+});
     }
 }
 
@@ -145,21 +152,30 @@ app.get("/ordinamentoLibri", function (request, result){
 })
 });
 
-// DELETE DI UN LIBRO TRAMITE PATH VARIABLE 
-app.delete('/libri/delete/:id', function(req,res,next){
-    
-    var id= req.params.id   
-        var query= `DELETE FROM libri WHERE id = "${id}"`
-        executeQuery(query,function(error,data){
-            if(error){
-                throw err;
-            }else{
-                result.json({utente: "Libro cancellato"}) 
-            }
-                })
-    })
- // MODIFICA DELLA PASSWORD DELL'UTENTE
+//DELETE TRAMITE ISBN DIMINUENDO IL NUMERO DI COPIE DA DEDICEDERE SE CANCELLARE DEFINITIVAMENTE IL RECORD O SEMPLICEMENTE IMPOSTARLO A 0.
+app.delete('/deleteLibri', async (req, res) => {
+    const ISBN = req.body.ISBN
+    async function countBookCopies(ISBN) {
+        const result = await executeQuery('SELECT COUNT(*) AS count FROM libri WHERE ISBN = ?', [ISBN]);
+        return result[0].count;
+    }
+    const numeroDiCopie = await countBookCopies(ISBN);
+    if (numeroDiCopie > 0) {
+        try {
+            await executeQuery('UPDATE libri SET numeroDiCopie = numeroDiCopie - 1 WHERE ISBN = ?;', [ISBN]);
+            const updatedNumeroDiCopie = await executeQuery('SELECT numeroDiCopie FROM libri WHERE ISBN = ?;', [ISBN]);
+            await executeQuery('UPDATE libri SET numeroDiCopie = 0 WHERE numeroDiCopie < 0 AND ISBN = ?;', [ISBN]); // Aggiunta condizione per impostare a 0 se inferiore a 0
+            res.send({ message: `Libro con ISBN ${ISBN} cancellato correttamente.Rimangono ancora: 0 copie` });
+        } catch (err) {
+            console.error('Errore di eliminazione', err);
+            return res.status(500).send({ error: `Si è verificato un errore durante l'eliminazione del libro con codice ISBN ${ISBN}: ${err.message}` });
+        }
+    } else {
+        res.send({ message: `Libro con ${ISBN} non trovato` });
+    }
+});
 
+  // MODIFICA DELLA PASSWORD DELL'UTENTE
 app.put('/modificaPassword', async (req, res) => {
     const username= req.body.Username;
     const email= req.body.Email;
@@ -180,6 +196,39 @@ app.put('/modificaPassword', async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: 'Si è verificato un errore' });
     }
+  });
+
+//ROUTH DI GESTIONE DEI PRESTITI CON RELAZIONE MOLTI A MOLTI 
+
+// Endpoint per aggiungere un prestito  
+app.post('/AggiuntaPrestiti', async (req, res) => {
+    const { ISBN, idUtenti } = req.body;
+    try {
+      const result = await executeQuery('INSERT INTO utentiLibri (idUtenti, ISBN) VALUES (?, ?)', [idUtenti, ISBN]);
+      res.send('Prestito aggiunto con successo');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Errore durante l aggiunta del prestito');
+    }
+  });
+
+  //Route di una lista di libri associati ad un untente 
+  app.get('/libri/:isbn/idUtenti/:id', (req, res) => {
+    const { isbn, id } = req.params;
+    // Definiamo la query MySQL
+    const query = 'SELECT * FROM utentiLibri WHERE isbn = ? AND idUtenti LIKE ? ';
+    // Insertiamo i parametri dinamicamente
+    const values = [isbn, `%${id}%`];
+    // Chiamiamo la funzione executeQuery con la nuova query ed i parametri appena definiti
+    executeQuery(query, values)
+      .then((results) => {
+        // Se tutto va bene, inviamo il risultato alla risposta
+        res.json(results);
+      })
+      .catch((error) => {
+        // Se si verifica un errore, segnaliamo lo stack trace
+        console.error('Error in executeQuery:', error.stack);
+      });
   });
 
 app.listen(23456);
