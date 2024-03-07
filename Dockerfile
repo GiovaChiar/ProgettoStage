98a0d501125d610bc65988a9de59932314d88704
Giovanni Chiarpenello <giovanni.chiarpenello@alten.it>
2024-03-07 17:37:09 +0100

    docker file e compose e k8s

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM node:18-slim
+FROM node:20-alpine
 #RUN sudo mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
 WORKDIR /home/node/app
-COPY package*.json ./
+ENV NODE_ENV=production
+COPY package*.json . /
+RUN npm install -g cors-anywhere@$version
 #RUN npm install cors
 RUN npm install
 COPY . .
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express= require ('express')
 const cors = require ('cors')
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+
+// port 
+const PORT= process.env.PORT || 8080
 
 const app= express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 // middleware
@@ -11,7 +18,7 @@ app.use(express.urlencoded({extended: true}))
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');    
     next();
 }*/
-//app.use(cors())
+
 /*app.use(cors({
     origin: '*',
     methods: '*',
@@ -20,16 +27,72 @@ app.listen(8080, function () {
   console.log('CORS abilitati')
 })*/
 //app.use(corsMiddleware);
-
-/*const corsOptions = {
-    origin: 'http://backend:8080', // Only allow from this domain
+/*
+const corsOptions = {
+    origin: 'http://frontend:4200', // Only allow from this domain
     methods: 'GET, POST, UPDATE, DELETE',                     // Specify allowed methods
-    //optionsSuccessStatus: 200                // For legacy browsers
-};*/
+    optionsSuccessStatus: 200                // For legacy browsers
+};
 
-app.use(cors()); 
+app.use(cors(corsOptions)); 
+
+app.createServer( function (request, response){     
+    console.log('request starting ' + request.url + '...');	//visualizzo nella console     
+    const headers = {         
+      'Access-Control-Allow-Origin': 'http://frontend:4200', // non mettere la slash finale        
+      'Access-Control-Allow-Headers': 'Content-Type,Authorization',         
+      'Access-Control-Allow-Methods': 'OPTIONS,GET,POST',         
+      'Access-Control-Max-Age': 86400, // 1 days * 24 ore * 60 minuti * 60 secondi        
+      'Vary': 'Origin, Access-Control-Request-Headers, Access-Control-Request-Method'    
+    };
+    console.log('---------------------------------');     
+    console.log(`Request [${request.method}] ${new Date()}`, request.headers);     
+    if (request.method === 'OPTIONS') {         
+      console.log('preflight request');         
+      response.writeHead(204, headers);         
+      response.end();         
+      return;     
+    }     
+    if (['GET', 'POST'].indexOf(request.method) > -1) {         
+      const content = { date: new Date() };         
+      console.log('content', content);         
+      response.writeHead(200, headers);         
+      response.end(JSON.stringify(content), 'utf-8');         
+      return;     
+    }     
+    response.writeHead(405, headers);     
+    response.end(`${request.method} is not allowed for the request.`);
+  }
+)*/
 
 
+/*http.createServer(function (request, response) {     
+  console.log('request starting ' + request.url + '...');	//visualizzo nella console     
+  const headers = {         
+    'Access-Control-Allow-Origin': 'http://frontend:4200', // non mettere la slash finale        
+    'Access-Control-Allow-Headers': 'Content-Type,Authorization',         
+    'Access-Control-Allow-Methods': 'OPTIONS,GET,POST',                
+    'Vary': 'Origin, Access-Control-Request-Headers, Access-Control-Request-Method'           
+  };     
+  console.log('---------------------------------');     
+  console.log(`Request [${request.method}] ${new Date()}`, request.headers);     
+  if (request.method === 'OPTIONS') {         
+    console.log('preflight request');         
+    response.writeHead(204, headers);         
+    response.end();         
+    return;     
+  }     
+  if (['GET', 'POST'].indexOf(request.method) > -1) {         
+    const content = { date: new Date() };         
+    console.log('content', content);         
+    response.writeHead(200, headers);         
+    response.end(JSON.stringify(content), 'application/json; charset=utf-8');         
+    return;     
+  }     
+  response.writeHead(405, headers);     
+  response.end(`${request.method} is not allowed for the request.`); 
+}).listen(PORT); //imposto il server per ascoltare sulla porta indicata
+console.log('Server running at http://localhost:' + PORT + '/');*/
 
 
 // routers
@@ -37,8 +100,7 @@ const router= require('./routes/UserRoutes.js')
 app.use('/', router)
 
 
-// port 
-const PORT= process.env.PORT || 8080
+
 
 //server 
 app.listen(PORT, ()=>{
